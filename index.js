17935f9f5d618851242e8835d674a8ad478747f7
Eryz <eryzer@gmail.com>
2019-08-26 00:25:45 +0700

    Modify index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(bodyParser.json())
 
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
         return res.send('Welcome to Mamiclone API, there are /api/v1/users and /api/v1/dorms resources to try')
     })
 
