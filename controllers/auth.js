0af214165dd6a317efdc8f7946edfbc628932b7e
Eryz <eryzer@gmail.com>
2019-08-26 10:42:08 +0700

    Modify auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken')
 const Sequelize = require('sequelize')
 
 const User = require('../models').user
-
 const Op = Sequelize.Op
 
 exports.login = (req, res) => {
@@ -30,17 +29,21 @@ exports.login = (req, res) => {
 exports.signup = (req, res) => {
     const { email, username } = req.body
 
-    User.findOrCreate({where: {[Op.or]:[{email}, {username}]}})
-        .then((user, created) => {
-            if (created) {
-                const token = jwt.sign({ id: user.id}, 'tautochrone', {expiresIn: "3 hours"})
-                let { id, username, email, createdAt } = user
-                res.status(201).send({ id, username, email, createdAt, token})
-            } else { 
-                res.status(409).send({
-                    message: 'Email/Username already exist!'
+    User.findOne({ where: {[Op.or]:[{email}, {username}]}})
+        .then(user => {
+            if(user) {
+                res.send({
+                    message: 'Email/Username already exist'
                 })
+            } else {
+                User.create(req.body)
+                    .then(user => {
+                        const token = jwt.sign({ id: user.id}, 'tautochrone', {expiresIn: "3 hours"})
+                        let { id, username, email, createdAt } = user
+                        res.send({ id, username, email, createdAt, token})
+                    })
             }
-            
         })
+
+    
 }
