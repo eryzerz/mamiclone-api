97f4178f963078eb1b62de63dcc099f2c58d9ef3
Eryz <eryzer@gmail.com>
2019-08-26 09:12:42 +0700

    Add success message after delete a record

--- controllers/user.js
+++ controllers/user.js
@@ -51,7 +51,9 @@ exports.delete = (req, res) => {
                 })
             }
             return user.destroy()
-                .then(() => res.status(204).send())
+                .then(() => res.status(204).send({
+                    message: `User with id ${req.params.id} has been deleted`
+                }))
                 .catch(error => res.status(400).send(error))
         })
         .catch(error => res.status(400).send(error))
