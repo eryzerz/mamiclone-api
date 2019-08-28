099238caa04ead18c762fd36ee25b07768e4f1f0
Eryz <eryzer@gmail.com>
2019-08-28 12:02:47 +0700

    Install Joi for input validation, implement it to Auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,24 @@
 const jwt = require('jsonwebtoken')
+const Joi = require('@hapi/joi')
 const Sequelize = require('sequelize')
+const { check, validationResult } = require('express-validator')
 
 const User = require('../models').user
 const Op = Sequelize.Op
 
 exports.login = (req, res) => {
+    const schema = {
+        email: Joi.string().email().required(),
+        password: Joi.string().regex(/^(?=.*[0-9]+.*)(?=.*[A-Z]+.*)(?=.*[!@#\$%\^\&\*\)\(\+\=\.\_\-]+.*)[0-9a-zA-Z!@#\$%\^\&*\)\(\+\=\.\_\-]{8,}$/).required(), 
+    }
+    
     const { email, password } = req.body
+    const result = schema.validate({email, password})
 
+    if(result.error) {
+        res.status(400).send(result.error.details[0].message)
+        return
+    }
     User.findOne({ where: {email, password}})
         .then(user => {
             if(user) {
@@ -28,7 +40,19 @@ exports.login = (req, res) => {
 
 
 exports.signup = (req, res) => {
-    const { email, username } = req.body
+    const schema = {
+        username: Joi.string().min(3).required(),
+        email: Joi.string().email(),
+        password: Joi.string().regex(/^(?=.*[0-9]+.*)(?=.*[A-Z]+.*)(?=.*[!@#\$%\^\&\*\)\(\+\=\.\_\-]+.*)[0-9a-zA-Z!@#\$%\^\&*\)\(\+\=\.\_\-]{8,}$/).required(),
+    }
+
+    const { email, username, password } = req.body
+    const result = schema.validate({email, username, password})
+
+    if(result.error) {
+        res.status(400).send(result.error.details[0].message)
+        return
+    }
 
     User.findOne({ where: {[Op.or]:[{email}, {username}]}})
         .then(user => {
